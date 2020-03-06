942b3c94fb5cb0bb38ff086c95925a1f4c087397
MAKSYM MALASHEV <s19227@pjwstk.edu.pl>
2020-03-06 09:29:42 +0100

    initial commit

---
+++ Tutorial1/Tutorial1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Tutoral1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var websiteUrl = args[0];
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(websiteUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var htmlContent = await response.Content.ReadAsStringAsync();
+
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                var emailAddresses = regex.Matches(htmlContent);
+
+                foreach (var e in emailAddresses)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
